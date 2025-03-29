6abbeecaf6f23e7ae0afe08b64ef329a767dff5f
Mitchell Fishman <tuq42483@temple.edu>
2025-03-29 17:26:11 -0400

    1st Commit-Defined variables and some functions needed for this project.

--- app/src/main/java/com/example/finalproject/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -10,11 +10,46 @@ import android.hardware.SensorManager;
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sensorManager: SensorManager
-    private var GPS: Sensor? = null
-    private var Temp: Sensor? = null
-    private var Microphone: Sensor? = null
-    private lateinit var text: TextView
+    private var locationSensor: Sensor? = null
+    private var tempSensor: Sensor? = null
+    private var soundSensor: Sensor? = null
 
+    private lateinit var locationTextView: TextView
+    private lateinit var tempTextView: TextView
+    private lateinit var soundTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager //developer.android.com
+        locationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+        temperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+        //soundSensor = sensorManager.getDefaultSensor(Sensor.TYPE_)
+
+        tempTextView = findViewById(R.id.tempTextView)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.reisterListener(this, it, SensorManager.SENSOR_PROXIMITY)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+
+        } else if (even.sensor.type == Sensor.TYPE_PROXIMITY) {
+
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
 
 
 }
